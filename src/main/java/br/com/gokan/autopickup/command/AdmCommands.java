7f73503e738f71e5831619d79b962276ad29fc4a
Gokan <138010470+GokanZin@users.noreply.github.com>
2024-07-30 11:21:05 -0300

    update

--- src/main/java/br/com/gokan/autopickup/command/AdmCommands.java
+++ src/main/java/br/com/gokan/autopickup/command/AdmCommands.java
@@ -1,11 +1,26 @@
 package br.com.gokan.autopickup.command;
 
+import br.com.gokan.autopickup.Main;
 import br.com.gokan.autopickup.utils.builders.CommandBuilder;
 
 public class AdmCommands {
 
-    void reloadCommand(){
-        new CommandBuilder("reload")
+    private Main main;
+    public AdmCommands(Main main) {
+        this.main = main;
+        Commands();
+    }
+
+
+    void Commands(){
+        new CommandBuilder("mautopickup")
                 .permissionMessage("Sem permissão para recarregar o plugin")
+                .aliases("recarregar")
+                .executor(( sender, args ) -> {
+                    main.reload();
+                    main.getPickupManager().reloadCaches(sender);
+                    sender.sendMessage("§aConfiguração foram recarregadas!");
+                    return true;
+                }).build();
     }
 }
